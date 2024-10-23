200d6817b92995736f7b460df55e7aca5c79fdc8
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-23 21:19:31 +0100

    Update files

---
+++ app/getPerm.js
@@ -0,0 +1,7 @@
+
+const issuerDidUri = 'did:dht:taqpsopukzrqq48rweaonqqp6ehf4mjpdb1k9bzkucqsboz4m4ty'
+const response  = await fetch(`https://vc-to-dwn.tbddev.org/authorize?issuerDid=${issuerDidUri}`);
+
+const data = await response.json();
+
+console.log(data)
