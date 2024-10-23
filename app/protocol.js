4b3be77b068cae2f987003b25d9363b02c0fd45a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-23 15:26:38 +0100

    Update files

---
+++ app/protocol.js
@@ -0,0 +1,99 @@
+import { Web5 } from "@web5/api";
+
+const { web5, did: myDid } = await Web5.connect();
+
+console.log(did)
+
+const protocolDefinition = {
+    "protocol": "https://social-media.xyz",
+    "published": true,
+    "types": {
+      "post": {
+        "schema": "https://social-media.xyz/schemas/postSchema",
+        "dataFormats": ["text/plain"]
+      },
+      "reply": {
+        "schema": "https://social-media.xyz/schemas/replySchema",
+        "dataFormats": ["text/plain"]
+      },
+      "image": {
+        "dataFormats": ["image/jpeg"]
+      },
+      "caption": {
+        "schema": "https://social-media.xyz/schemas/captionSchema",
+        "dataFormats": ["text/plain"]
+      }
+    },
+    "structure": {
+      "post": {
+        "$actions": [
+          {
+            "who": "anyone",
+            "can": ["create", "read"]
+          }
+        ],
+        "reply": {
+          "$actions": [
+            {
+              "who": "recipient",
+              "of": "post",
+              "can": ["create"]
+            },
+            {
+              "who": "author",
+              "of": "post",
+              "can": ["create"]
+            }
+          ]
+        }
+      },
+      "image": {
+        "$actions": [
+          {
+            "who": "anyone",
+            "can": ["create", "read"]
+          }
+        ],
+        "caption": {
+          "$actions": [
+            {
+              "who": "anyone",
+              "can": ["read"]
+            },
+            {
+              "who": "author",
+              "of": "image",
+              "can": ["create"]
+            }
+          ]
+        },
+        "reply": {
+          "$actions": [
+            {
+              "who": "author",
+              "of": "image",
+              "can": ["read"]
+            },
+            {
+              "who": "recipient",
+              "of": "image",
+              "can": ["create"]
+            }
+          ]
+        }
+      }
+    }
+}
+
+
+
+const { protocol, status } = await web5.dwn.protocols.configure({
+    message: {
+      definition: protocolDefinition
+    }
+});
+
+//sends protocol to remote DWNs immediately (vs waiting for sync)
+await protocol.send('did:dht:taqpsopukzrqq48rweaonqqp6ehf4mjpdb1k9bzkucqsboz4m4ty').then(() => {
+    console.log("Protocols install success!")
+})
