3c824f3cc9dcf39e34d532834c9194a45a75ee92
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2024-10-23 14:18:12 +0100

    Update files

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 // did:dht
-import { DidDht } from '@web5/dids'
+import { DidDht } from '@web5/dids';
 
 //did:jwk
-import { DidJwk } from '@web5/dids'
+import { DidJwk } from '@web5/dids';
+import { Web5 } from '@web5/api';
 
 
 // Creates a DID using the DHT method and publishes the DID Document to the DHT
@@ -14,7 +15,29 @@ const portableDid = await didDht.export();
 console.log(portableDid);
 
 // DID string
-const did = didDht.uri;
+// const did = didDht.uri;
 
 // DID Document
 const didDocument = JSON.stringify(didDht.document);
+
+const {did, web5} = await Web5.connect({
+  didCreateOptions: {
+    // Use community DWN instance hosted on Google Cloud
+    dwnEndpoints: ['https://dwn.gcda.xyz']
+  },
+  registration: {
+    onSuccess: () => {
+      /*
+      Registration succeeded, set a local storage value to indicate the user 
+      is registered and registration does not need to occur again
+      */
+     console.log("Registraction Succeed!")
+    },
+    onFailure: (error) => {
+      /* 
+      Registration failed, display an error message to the user, and pass in 
+      the registration object again to retry next time the user connects
+      */
+    },
+  },
+})
